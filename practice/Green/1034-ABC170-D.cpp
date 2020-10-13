f8291770abad841d1b5c365fa8bb562e1304dbfc
kyapikyapi <steinein46@gmail.com>
2020-10-13 11:07:09 +0900

    add ABC170-D

--- practice/Green/1034-ABC170-D.cpp
+++ practice/Green/1034-ABC170-D.cpp
@@ -8,27 +8,27 @@ int cnt[1000005];
 
 int main(){
 
-	int n;
+    int n;
     std::cin >> n;
 
-	for(int i = 0; i < n; ++i){
-		int x;
+    for(int i = 0; i < n; ++i){
+        int x;
         std::cin >> x;
-		cnt[x]++;
-	}
+        cnt[x]++;
+    }
 
-	int ans = 0;
+    int ans = 0;
 
-	for(int i = 1; i <= 1000000; ++i){
-		if(cnt[i] == 0) continue;
+    for(int i = 1; i <= 1000000; ++i){
+        if(cnt[i] == 0) continue;
 
-		for(int j = i + i; j <= 1000000; j += i){
-			cnt[j] = 2;
-		}
-		if(cnt[i] == 1) ++ans;
-	}
+        for(int j = i + i; j <= 1000000; j += i){
+            cnt[j] = 2;
+        }
+        if(cnt[i] == 1) ++ans;
+    }
 
-	std::cout << ans << std::endl;
+    std::cout << ans << std::endl;
 
     return 0;
 }
