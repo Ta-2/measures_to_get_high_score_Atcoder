4f07b631d9ed13250a92def7dda043a945cde5f6
たつや <54496533+Ta-2@users.noreply.github.com>
2024-04-24 00:30:45 +0900

    Update Ta_2_template.cpp

--- technique/template/Ta_2_template.cpp
+++ technique/template/Ta_2_template.cpp
@@ -24,14 +24,16 @@ typedef long double ld;
 #define v(Type) vector<Type>
 #define vv(Type) vector<vector<Type>>
 #define vvv(Type) vector<vector<vector<Type>>>
-#define vini(n, m) ((n), (m))
+#define v_i(Type, d_v, d_n) v(Type) d_v(d_n)
+#define vv_i(Type, d_v, d_m, d_n) vv(Type) d_v(d_m, v(Type)(d_n))
+#define vvv_i(Type, d_v, d_l, d_m, d_n) vvv(Type) d_v (d_l, vv(Type)(d_m, v(Type)(d_n)))
 //------------forマクロ定義------------
-//#define rep(i, n) for(int i = 0; (i) < (n); (i)++)
-#define rep(d_i, d_n) for(int d_i = 0; d_i < d_n; d_i++)
-//#define rep2(i, m, n) for(int i = (m); (i) < (n); i++)
-#define rep2(d_i, d_m, d_n) for(int (d_i) = (d_m); (d_i) < (d_n); d_i++)
-//#define repr(i, n) for(int i = n; i >= 0; i--)
-#define repr(d_i, d_n) for(int (d_i) = (d_n); (d_i) >= 0; (d_i)--)
+#define rep1(d_i, d_n) for(int d_i = 0; d_i < d_n; d_i++)
+#define rep2(d_i, d_m, d_n) for(int d_i = d_m; d_i < d_n; d_i++)
+#define rep3(d_i, d_l, d_m, d_n) for(int d_i = d_l; d_i < d_m; d_i+=d_n)
+#define rep_overload3(a, b, c, d, e, ...) e
+#define rep(...) rep_overload3(__VA_ARGS__, rep3, rep2, rep1)(__VA_ARGS__)
+
 #define fe(d_i, STL) for(auto d_i: (STL))
 //------------ioマクロ定義------------
 #define co cout<<
@@ -39,20 +41,37 @@ typedef long double ld;
 #define in cin>>
 #define br co "" en
 #define pr(value) ","<<#value<<": "<<value
-#define sn(value) #value,value
+#define sn(value) #value, value
 
-#define in_v(type, d_v) type d_v;\
-                        in d_v;
-#define in_l(type, d_l, d_v)  v(type) d_l( d_v );\
-                              rep(d_i, d_v) in(d_l[d_i]);
-#define in_vl(type_v, d_v, type_l, d_l) \
-                  in_v(type_v, d_v);\
-                  in_l(type_l, d_l, d_v);
+template<class... T>
+void input(T&... a){
+  (cin >> ... >> a);
+}
+#define in_v(type, ...)\
+    type __VA_ARGS__;\
+    input(__VA_ARGS__);
+
+#define in_l(type, d_l, d_v)\
+    v(type) d_l( d_v );\
+    rep(d_i, d_v) in(d_l[d_i]);
+    
+#define in_vl(type_v, d_v, type_l, d_l)\
+    in_v(type_v, d_v);\
+    in_l(type_l, d_l, d_v);
 
 //------------表示マクロ定義------------
-template<class Type>
-void print_value(string name, Type value){co name << ":" << value en;}
-#define p(value) print_value(sn(value))
+void print_name(string name){
+  co name << ": ";
+}
+template<class T, class... Ts>
+void print_value(T head, Ts... body){
+  co head;
+  (cout << ... << (cout << ' ' , body));
+  br;
+}
+#define p(...)\
+    print_name(#__VA_ARGS__);\
+    print_value(__VA_ARGS__);
 
 template<class Type>
 void print_value2(string name, Type value){
