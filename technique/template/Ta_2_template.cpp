ec4977740524e48a117a38c95f635c890983fd0f
たつや <54496533+Ta-2@users.noreply.github.com>
2024-04-24 11:11:26 +0900

    Update Ta_2_template.cpp

--- technique/template/Ta_2_template.cpp
+++ technique/template/Ta_2_template.cpp
@@ -1,7 +1,7 @@
 //------------ヘッダーインクルード------------
 #include <bits/stdc++.h>
 #include <stdio.h>
-#include <cmath>
+//#include <cmath>
 #include <stdarg.h>
 #include <iostream>
 #include <vector>
@@ -12,21 +12,35 @@
 #include <stack>
 #include <queue>
 #include <algorithm>
+#include <limits.h>
 using namespace std;
 //------------型マクロ定義------------
 typedef long l;
 typedef long long ll;
 typedef long double ld;
-#define INIF 1e9;
+#define INF 1e9;
 #define MOD 1000000007;
 #define ALL(v) (v).begin(), (v).end()
 //------------vectorマクロ定義------------
 #define v(Type) vector<Type>
 #define vv(Type) vector<vector<Type>>
 #define vvv(Type) vector<vector<vector<Type>>>
-#define v_i(Type, d_v, d_n) v(Type) d_v(d_n)
-#define vv_i(Type, d_v, d_m, d_n) vv(Type) d_v(d_m, v(Type)(d_n))
-#define vvv_i(Type, d_v, d_l, d_m, d_n) vvv(Type) d_v (d_l, vv(Type)(d_m, v(Type)(d_n)))
+
+#define v_i1(Type, d_v, d_n)        v(Type) d_v(d_n)
+#define v_i2(Type, d_v, d_n, v_ini) v(Type) d_v(d_n, v_ini)
+#define v_i_overload2(a, b, c, d, e, ...) e
+#define v_i(...) rep_overload3(__VA_ARGS__, v_i2, v_i1)(__VA_ARGS__)
+
+#define vv_i1(Type, d_v, d_m, d_n)        vv(Type) d_v(d_m, v(Type)(d_n))
+#define vv_i2(Type, d_v, d_m, d_n, v_ini) vv(Type) d_v(d_m, v(Type)(d_n, v_ini))
+#define vv_i_overload2(a, b, c, d, e, f, ...) f
+#define vv_i(...) vv_i_overload2(__VA_ARGS__, vv_i2, vv_i1)(__VA_ARGS__)
+
+#define vvv_i1(Type, d_v, d_l, d_m, d_n)        vvv(Type) d_v (d_l, vv(Type)(d_m, v(Type)(d_n)))
+#define vvv_i2(Type, d_v, d_l, d_m, d_n, v_ini) vvv(Type) d_v (d_l, vv(Type)(d_m, v(Type)(d_n, v_ini)))
+#define vvv_i_overload2(a, b, c, d, e, f, g, ...) g
+#define vvv_i(...) vvv_i_overload2(__VA_ARGS__, vvv_i2, vvv_i1)(__VA_ARGS__)
+
 //------------forマクロ定義------------
 #define rep1(d_i, d_n) for(int d_i = 0; d_i < d_n; d_i++)
 #define rep2(d_i, d_m, d_n) for(int d_i = d_m; d_i < d_n; d_i++)
@@ -51,13 +65,24 @@ void input(T&... a){
     type __VA_ARGS__;\
     input(__VA_ARGS__);
 
-#define in_l(type, d_l, d_v)\
+#define in_l1(type, d_l, d_n)\
+    v(type) d_l( d_n );\
+    rep(d_i, d_n) in(d_l[d_i]);
+#define in_l2(type, d_l, d_m, d_n)\
+    vv_i(type, d_l, d_m ,d_n);\
+    rep(d_i, d_m){\
+      rep(d_j, d_n) in(d_l[d_i][d_j]);\
+    }
+#define in_l_overload2(a, b, c, d, e, ...) e
+#define in_l(...) in_l_overload2(__VA_ARGS__, in_l2, in_l1)(__VA_ARGS__)
+    
+#define in_l3(type, d_l, d_v)\
     v(type) d_l( d_v );\
     rep(d_i, d_v) in(d_l[d_i]);
     
 #define in_vl(type_v, d_v, type_l, d_l)\
     in_v(type_v, d_v);\
-    in_l(type_l, d_l, d_v);
+    in_l1(type_l, d_l, d_v);
 
 //------------表示マクロ定義------------
 void print_name(string name){
@@ -74,14 +99,27 @@ void print_value(T head, Ts... body){
     print_value(__VA_ARGS__);
 
 template<class Type>
-void print_value2(string name, Type value){
-  int num = 0;
-  //fe(i, value)co name << "[" << num++ << "]:"  << i << ", ";
+void print_v(string name, Type v){
   co name << ": ";
-  fe(i, value)co i << ", ";
+  fe(i, v)co i << ", ";
   br;
 }
-#define p_l(Type, value) print_value2<Type>(sn(value))
+#define p_v(value) print_v(sn(value))
+
+template<class Type>
+void print_vv(string name, Type vv){
+  co name << ": " en;
+  co "{" en;
+  fe(v, vv){
+    co "  {";
+    fe(i, v){
+      co i << " ";
+    }
+    co "}" en;
+  }
+  co "}" en;
+}
+#define p_vv(value) print_vv(sn(value))
 
 void print_value3(string name, auto value){
   int num = 0;
